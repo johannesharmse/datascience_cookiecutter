324bfd8b55f86516790a39d226f8b138ff0da753
Johannes Harmse <johannesharmse91@gmail.com>
2017-11-17 14:46:57 -0800

    Add folders, clean code and add comments

--- cookiecutter.sh
+++ cookiecutter.sh
@@ -6,31 +6,35 @@
 
 # Script that creates a generic data science project folder structure.
 
-# arg_1="5"
-# p_out="Awe"
-dest_out="Root"
-license_out="Templates/LICENSE/GNU_v3.md"
+# dest_out: destination folder in string format. Use "../" to go up a level.
 
-while getopts ":a:p:l:" opt; do
+dest_out="Root" # default destination folder
+license_out="Templates/LICENSE/GNU_v3.md" #default license template
+
+# get arguments if any has been entered
+
+while getopts ":d:l:" opt; do
   case $opt in
-    a) arg_1="$OPTARG"
-    ;;
-    p) dest_out="$OPTARG"
+    d) dest_out="$OPTARG"
     ;;
     l) license_out="$OPTARG"
     ;;
-    \?) echo "Invalid option -$OPTARG" >&3
+    \?) echo "Invalid option -$OPTARG" >&2
     ;;
   esac
 done
 
-if [ $dest_out=="Root" ] && [ ! -d $dest_out ]
+# create destination folder if it does not exist
+
+if [ ! -d $dest_out ]
 then
   mkdir $dest_out
 fi
 
-cp $license_out "$dest_out/LICENSE.md"
+# create folder structure and files
 
-# printf "Argument p_out is %s\n" "$p_out"
-# printf "Argument arg_1 is %s\n" "$arg_1"
-# printf "Argument l_out is %s\n" "$l_out"
+cp $license_out "$dest_out/LICENSE.md"
+mkdir "$dest_out/data"
+mkdir "$dest_out/docs"
+mkdir "$dest_out/results"
+mkdir "$dest_out/src"
